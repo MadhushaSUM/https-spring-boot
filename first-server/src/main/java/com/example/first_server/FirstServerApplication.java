0271f2826878cb37fc4395159b854cf10ae1ebdc
Madhusha Laksitha <madhusha.sum@gmail.com>
2025-07-23 18:37:09 +0530

    first server initial code with simple GET endpoint

---
+++ first-server/src/main/java/com/example/first_server/FirstServerApplication.java
@@ -0,0 +1,21 @@
+package com.example.first_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController("/")
+public class FirstServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstServerApplication.class, args);
+	}
+
+	@GetMapping("hello")
+	public String sayHello() {
+		return "Hello from first server";
+	}
+
+}
